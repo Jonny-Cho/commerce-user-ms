021580559e761dd56c426a10bcb77f1a164beca3
Jonny-Cho <andante2183@gmail.com>
2022-01-18 22:10:14 +0900

    feat: spring cloud config 적용

--- src/main/java/com/commerce/security/WebSecurity.java
+++ src/main/java/com/commerce/security/WebSecurity.java
@@ -24,7 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
-            .hasIpAddress("220.118.249.168")
+            .hasIpAddress("220.76.245.250")
             .and()
             .addFilter(getAuthenticationFilter());
 
